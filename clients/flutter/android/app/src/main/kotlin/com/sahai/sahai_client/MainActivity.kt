2b3630a82ced7711b55787e37a6a11e1369057ea
Uni-coder-harsh <harsh06264@gmail.com>
2026-06-08 17:19:43 +0530

    chore: Convert sub-directories to Git submodules pointing to remote GitHub repos

--- clients/flutter/android/app/src/main/kotlin/com/sahai/sahai_client/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sahai.sahai_client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- services/api-node/src/routes/curriculum.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const curriculumController = require('../controllers/curriculum.controller');
-
-// Expose curriculum structure nodes and links
-router.get('/:domain', curriculumController.getCurriculum);
-
-module.exports = router;
